641e4ba8dae926c564e659a2e44aca812496ac47
Enrico Sada <enrico@sada.io>
2018-05-04 15:06:42 +0200

    make sample project a bit more complicated
    - no args => hello world
    - `--port 8083` => suave app on `http://0.0.0.0:8083/`

--- src/c1/Program.fs
+++ src/c1/Program.fs
@@ -2,8 +2,40 @@
 
 open System
 
+open Argu
+
+type CLIArguments =
+    | Port of tcp_port:int
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Port _ -> "specify a primary port."
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
     l1.Say.hello "built paket .net core"
-    0 // return an integer exit code
+
+    let parser = ArgumentParser.Create<CLIArguments>(programName = "c1")
+
+    try
+        let args = parser.Parse argv
+
+        let port =
+            match args.GetAllResults() with
+            | [Port p] -> Some (uint16 p)
+            | _ -> None
+
+        port
+        |> Option.iter (l1.Say.start)
+
+        0
+    with
+    | :? ArguParseException as ex ->
+        printfn "%s" ex.Message
+        1
+    | ex ->
+        printfn "Internal Error:"
+        printfn "%s" ex.Message
+        2
--- src/l1/Library.fs
+++ src/l1/Library.fs
@@ -1,5 +1,13 @@
 namespace l1
 
+open Suave
+
 module Say =
     let hello name =
         printfn "Hello %s" name
+
+    let start port =
+        startWebServer
+            { defaultConfig with
+                bindings = [ HttpBinding.create HTTP System.Net.IPAddress.Any port ] }
+            (Successful.OK "Hello World!")
