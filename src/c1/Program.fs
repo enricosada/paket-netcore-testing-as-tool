a90b758132ba76f11fe525786261e584da6c234a
Enrico Sada <enrico@sada.io>
2018-05-04 13:16:10 +0200

    add project

---
+++ src/c1/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    l1.Say.hello "built paket .net core"
+    0 // return an integer exit code
